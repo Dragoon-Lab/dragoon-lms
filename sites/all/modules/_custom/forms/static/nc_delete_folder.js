0e3a9183d357ea71c26e4ca06567c2f0893195ea
Dharani <dsakaryp@asu.edu>
2017-03-02 14:47:47 -0700

    Initial Changes - Unshare folder/ Repurpose Delete

--- sites/all/modules/_custom/forms/static/nc_delete_folder.js
+++ sites/all/modules/_custom/forms/static/nc_delete_folder.js
@@ -7,8 +7,10 @@ jQuery(document).ready(function($) {
         console.log("now I can show the expanded private and shared list");
         $("h2[data-folder='private']").next().slideDown();
         $("h2[data-folder='private']").addClass("current");
-        $("h2[data-folder='shared']").next().slideDown();
-        $("h2[data-folder='shared']").addClass("current");
+        $("h2[data-folder='shared_by_me']").next().slideDown();
+        $("h2[data-folder='shared_by_me']").addClass("current");
+        $("h2[data-folder='shared_with_me']").next().slideDown();
+        $("h2[data-folder='shared_with_me']").addClass("current");
     });
 
 
@@ -17,19 +19,35 @@ jQuery(document).ready(function($) {
         //program to decode the deleted checks and sending
         var folders = [];
         var models = {};
+        var sharedFolders =[];
         var owner=$('#dragoon_ncDelFolder_form input[name=owner]').val();
+        
         $("#dragoon_ncDelFolder_form input:checkbox:checked").each(function(key,value) {
             var str = value.name;
             console.log(str);
             var str_array=str.split('-');
             if(str_array[0]=="fo"){
                 //push intlo folder array
-                 var filter = str_array[1];
-                if(filter == "private")
+                var filter = str_array[1];
+                
+                if(filter == "private"){
                     filter = owner+"-private";
-                else
-                    filter = filter+"-"+owner;
-                folders.push(filter);
+                    folders.push(filter);
+                }else{
+                    // shared folder
+                    if(filter.search(" by ") >=0 && filter.search(" by ")+4 < filter.length){
+                        var folder_owner = filter.substring(filter.search(" by ")+4);
+                    }
+                    if ( folder_owner && folder_owner !== ""){
+                        filter = filter.substring(0,filter.search(" by "))+"-"+folder_owner;
+                        sharedFolders.push(filter);
+                    }else{
+                        filter = filter+"-"+owner;  
+                        folders.push(filter);  
+                    }
+                    
+                }
+
             }
             else if(str_array[0]=="mo"){
                 var filter = str_array[3];
@@ -48,14 +66,18 @@ jQuery(document).ready(function($) {
         if(Object.keys(models).length>0){
             data.models = models;
         }
+        if(Object.keys(sharedFolders).length>0){
+            data.sharedFolders = sharedFolders;
+        }
 
         data.owner = owner;
         data.req_type = 'delete Folder';
         console.log(data);
         $.post('sites/all/modules/_custom/NC_models/static/nonClassUpdates.php', data)
             .success(function (datum) {
-                console.log("received",datum);
+                //console.log("received",datum);
                 location.reload();
          });
+         
     });
 });
